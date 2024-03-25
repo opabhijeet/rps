706f7d57af31e8e24199ffe04312e4015a0895ba
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-03-26 03:20:20 +0530

    add main playGame function handle invalid choice

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function getComputerChoice(){
     return "scissors";
 }
 function result(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
     if(playerSelection === computerSelection) return "It's a tie";
     if(playerSelection === "rock"){
         if(computerSelection === "paper") return "You Lose! Paper beats Rock";
@@ -15,8 +14,33 @@ function result(playerSelection, computerSelection){
         if(computerSelection === "scissors") return "You Lose! Scissors beat Paper";
         else return "You Win! Paper beats Rock";
     }
-    else{
+    else if(playerSelection === "scissors"){
         if(computerSelection === "rock") return "You Lose! Rock beats Scissors";
         else return "You Win! Scissors beat Paper";
     }
+    else{
+        console.log("Invalid choice. Try again.");
+        playerSelection = prompt("Enter your choice").toLowerCase();
+        return result(playerSelection, computerSelection);
+    }
 }
+
+function playGame(){
+    let computerScore = 0;
+    let playerScore = 0;
+    for(let i=1;i<=3;i++){
+        let playerSelection = prompt("Enter your choice").toLowerCase();
+        let computerSelection = getComputerChoice();
+        let winner = result(playerSelection, computerSelection);
+        if(winner.includes("Win")) playerScore++ ;
+        else if(winner.includes("Lose")) computerScore++;
+        console.log(winner);
+    }
+    if(playerScore < computerScore) console.log("You Lose!");
+    else if(playerScore > computerScore) console.log("You Win!")
+    else console.log("Tie!");
+
+    console.log(`Score: ${playerScore} - ${computerScore}`);
+}
+
+playGame();
